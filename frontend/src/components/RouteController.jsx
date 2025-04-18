9665f2efa380f75443f78eb7c7c3a0f4d878e4b9
syeero7 <EmalHasitha@proton.me>
2025-04-18 14:50:11 +0530

    refactor: replace layouts with RouteController

---
+++ frontend/src/components/RouteController.jsx
@@ -0,0 +1,28 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+
+function RouteController({ type }) {
+  const { user } = useAuth();
+
+  switch (type) {
+    case "public":
+      return !user ? <Outlet /> : <Navigate to="/chat" replace />;
+
+    case "private":
+      return user ? <Outlet /> : <Navigate to="/" replace />;
+  }
+}
+
+RouteController.propTypes = {
+  type: (props, propName, componentName) => {
+    const property = props[propName];
+
+    if (property !== "PUBLIC" && property !== "PRIVATE") {
+      throw new Error(
+        `Invalid prop ${propName} passed to ${componentName}. Expected either "public" or "private"`
+      );
+    }
+  },
+};
+
+export default RouteController;
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import RouteController from "./components/RouteController";
+
 import Home from "./pages/Home";
 import Signin from "./pages/Sign-in";
 import Signup from "./pages/Sign-up";
-import AuthLayout from "./Layouts/AuthLayout";
 
 const router = createBrowserRouter([
   {
@@ -12,12 +13,16 @@ const router = createBrowserRouter([
     Component: App,
     children: [
       {
-        Component: AuthLayout,
+        element: <RouteController type="public" />,
         children: [
           { index: true, Component: Home },
           { path: "sign-in", Component: Signin },
           { path: "sign-up", Component: Signup },
         ],
+      },
+      {
+        element: <RouteController type="private" />,
+        children: [],
       },
     ],
   },
