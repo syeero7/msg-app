e9e99906166818c6c5a8395c924c41bcba5acc92
syeero7 <EmalHasitha@proton.me>
2025-04-18 06:00:58 +0530

    test: remove all tests

--- backend/tests/setup.js
+++
@@ -1,6 +0,0 @@
-import { beforeEach } from "vitest";
-import resetDb from "./utils/reset-db.js";
-
-beforeEach(async () => {
-  await resetDb();
-});
--- backend/tests/utils/reset-db.js
+++
@@ -1,9 +0,0 @@
-import prisma from "../../src/config/prisma-client.js";
-
-export default async () => {
-  await prisma.$transaction([
-    prisma.message.deleteMany(),
-    prisma.group.deleteMany(),
-    prisma.user.deleteMany(),
-  ]);
-};
