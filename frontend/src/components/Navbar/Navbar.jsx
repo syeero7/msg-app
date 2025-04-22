364cd40acc492d5ded254b6238388afe99e0571d
syeero7 <EmalHasitha@proton.me>
2025-04-22 21:24:43 +0530

    feat: add Navbar component

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { LogOut, Users, LayoutGrid, UserPen } from "lucide-react";
+import { NavLink, useMatch } from "react-router-dom";
+import { useAuth } from "../AuthProvider";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  const match = useMatch("/chat/users/:pattern");
+  const { onLogout } = useAuth();
+
+  const handleClick = async () => {
+    if (window.confirm("Are you sure you want to log out?")) {
+      onLogout();
+    }
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink
+            to="/chat/users/all"
+            className={`${styles.item} ${match ? "active" : ""}`}>
+            <Users />
+            <span>Users</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/chat/groups" className={styles.item}>
+            <LayoutGrid />
+            <span>Groups</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/profile/edit" className={styles.item}>
+            <UserPen />
+            <span>Edit profile</span>
+          </NavLink>
+        </li>
+
+        <li>
+          <button onClick={handleClick} className={styles.item}>
+            <LogOut />
+            <span>Logout</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
